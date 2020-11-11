a34c848365b1ec5bfdebb71e8fb3918fd22d48d4
Rolando Urquiza <rolurquiza@gmail.com>
2020-11-11 11:44:13 +0200

    New backup 2020-11-11 11:44:13

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp $HOME/{.bashrc,.gitconfig} .
+cp $HOME/{.bashrc,.gitconfig,.vimrc} .
 cp -r $HOME/{.i3,.xmonad} .
 cp -r $HOME/.config/{dunst,rofi,polybar} .config
 
@@ -9,10 +9,10 @@ gs="$(git status | grep -i "modified")"
 
 # If there is a new change
 if [[ $gs == *"modified"* ]]; then
-  echo "push"
+  # push to Github
+  git add -u;
+  git commit -m "New backup `date +'%Y-%m-%d %H:%M:%S'`";
+  git push origin master echo "push"
 fi
 
-# push to Github
-git add -u;
-git commit -m "New backup `date +'%Y-%m-%d %H:%M:%S'`";
-git push origin master
+
