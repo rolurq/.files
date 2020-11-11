d3fbbfa81210a80d639cb9477bf5654c89dd6296
Rolando Urquiza <rolurquiza@gmail.com>
2020-11-11 11:45:50 +0200

    New backup 2020-11-11 11:45:50

--- backup.sh
+++ backup.sh
@@ -12,7 +12,6 @@ if [[ $gs == *"modified"* ]]; then
   # push to Github
   git add -u;
   git commit -m "New backup `date +'%Y-%m-%d %H:%M:%S'`";
-  git push origin master echo "push"
 fi
 
-
+git push origin master
